1cfc734ef39e3fcc2ede05778e2687e9b2091939
Jonathan Dickerson <jongdickerson@gmail.com>
2015-03-03 08:14:00 -0500

    basic js file structure complete

---
+++ app/scripts/ui/menu.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Menu = React.createClass({
+	render: function () {
+		return (
+			<div className="menu-container">
+      </div>
+		);
+	}
+});
+
+module.exports = Menu;
