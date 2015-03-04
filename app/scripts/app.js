b059dc128c79ce1acb412b465b8cb1adbdb7dc21
Jonathan Dickerson <jongdickerson@gmail.com>
2015-03-04 07:53:13 -0500

    added some interactivity on splash page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,11 +8,33 @@ var React = window.React = require('react'),
     mountNode = document.getElementById("app");
 
 var App = React.createClass({
+  getInitialState: function() {
+    return {
+      showMenu: false
+    };
+  },
+  onShowHide: function() {
+    this.setState({
+      showMenu: !this.state.showMenu
+    });
+  },
   render: function() {
     return (
       <div className="app-container">
-        <Hero />
-        <Menu />
+        <div className="hero-container" style={{height: this.state.showMenu ? "95vh" : "100vh"}}>
+          <Hero />
+          <div className="menu-link" onClick={this.onShowHide}>
+            <span>M</span>
+            <span>O</span>
+            <span>R</span>
+            <span>E</span>
+            <span>M</span>
+            <span>E</span>
+            <span>O</span>
+            <span>W</span>
+          </div>
+        </div>
+        <Menu show={this.state.showMenu}/>
         <Content />
       </div>
     );
